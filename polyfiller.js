1dabe1b4261a34463cc309b9897fc0936f693523
Rick Lancee <rickondraw@gmail.com>
2016-11-09 12:28:46 +0100

    inital commit

---
+++ polyfiller.js
@@ -0,0 +1,70 @@
+'use strict';
+
+window.polyfiller = (function () {
+  var polyFillsLoadedEvent = new CustomEvent('polyfillsLoaded', {
+    bubbles: true
+  });
+
+  var polyfillsLoaded = false;
+  var toBeLoadedPolyfills = [];
+
+  var loadScripts = function loadScripts(urls, succesCB, failCB) {
+    var count = urls.length;
+    var failed = false;
+
+    if (urls.length == 0) return succesCB();
+
+    urls.forEach(function (url) {
+      var script = document.createElement('script');
+
+      script.onload = function () {
+        if (failed) return;
+        if (! --count) succesCB();
+      };
+
+      script.onerror = function () {
+        if (failed) return;
+        failCB();
+        failed = true;
+      };
+
+      script.src = url;
+      document.head.insertBefore(script, document.head.firstChild);
+    });
+  };
+
+  var load = function() {
+    if (polyfillsLoaded) {
+      return;
+    }
+    polyfillsLoaded = true;
+
+    loadScripts(toBeLoadedPolyfills, function () {
+      requestAnimationFrame(function () {
+        document.dispatchEvent(polyFillsLoadedEvent);
+      });
+    }, function () {
+      polyfillsLoaded = false;
+      throw new Error('Failed to load required polyfills');
+    });
+  };
+
+  var addPolyfill = function(url) {
+    if (Object.prototype.toString.call(url) === '[object Array]') {
+      url.forEach(function(url) {
+        toBeLoadedPolyfills.push(url);
+      });
+      return;
+    }
+    toBeLoadedPolyfills.push(url);
+  };
+
+  return {
+    load: load,
+    addPolyfill: addPolyfill,
+    test: function(tests) {
+      tests.call({addPolyfill: addPolyfill});
+      load();
+    }
+  };
+})();
